21a982ced4462a01a347f17c2927a156c573ab86
Mohamed Mansour <hello@mohamedmansour.com>
2010-11-09 22:15:33 -0500

    Add Haptic pointer previewer using HTML5 Canvas.

--- js/runner_factory.js
+++ js/runner_factory.js
@@ -4,13 +4,17 @@
  * @param {HTMLElement} opt_bkg An optional param that points to the current
  *                              background page.
  */
-RunnerFactory = function(opt_bkg) {
-  this.bkg_ = opt_bkg || chrome.extension.getBackgroundPage();
-  this.haptics_ = this.bkg_.plugin;
+RunnerFactory = function(haptics, renderer) {
+  this.console_ = chrome.extension.getBackgroundPage().console;
+  this.haptics_ = haptics;
+  this.renderer_ = renderer;
+  this.ctx_ = renderer.getContext('2d');
   this.runners_ = {};
   this.worker_ = null;
-  this.interval_ = null;
+  this.haptic_interval_ = null;
+  this.render_interval_ = null;
   this.current_runner_ = null;
+  this.position_ = null;
 };
 
 /**
@@ -111,16 +115,20 @@ RunnerFactory.prototype._onForce = function(force) {
  */
 RunnerFactory.prototype._onStart = function() {
   this._debug('Started ' + this.current_runner_);
-  this.interval_ = setInterval(function() {
-    this._onHapticLoop();
-  }.bind(this), 1, this);
+  
+  // Haptic loop runs every 1ms.
+  this.haptic_interval_ = setInterval(this._onHapticLoop.bind(this), 1, this);
+  
+  // Renderer loop runs every 30 ms.
+  this.render_interval_ = setInterval(this._onRenderLoop.bind(this), 30, this);
 };
 
 /**
  * @private
  */
 RunnerFactory.prototype._onStop = function() {
-  clearInterval(this.interval_);
+  clearInterval(this.haptic_interval_);
+  clearInterval(this.render_interval_);
   this.haptics_.sendForce([0.0, 0.0, 0.0]);
   this.worker_.removeEventListener('message', this._onMessage, false);
   this.worker_ = null;
@@ -128,11 +136,49 @@ RunnerFactory.prototype._onStop = function() {
 };
 
 /**
- *
+ * Haptics loop that runs ever 1ms.
  * @private
  */
 RunnerFactory.prototype._onHapticLoop = function() {
-  this.post({cmd: 'update', position: this.haptics_.position});
+  this.position_ = this.haptics_.position;
+  this.post({cmd: 'update', position: this.position_});
+};
+
+/**
+ * Renderer loop that runs ever 1ms.
+ * @private
+ */
+RunnerFactory.prototype._onRenderLoop = function() {
+  // Temp gfx, for testing performance. Just to test stuff up.
+  // We need to somehow figure out a proper design on how to render various
+  // examples, since the haptic rendering happens within a Worker which doesn't
+  // have access to the outside world unless through messaging. We don't want
+  // to access the haptic logic worker since it might slow things down.
+  
+  // Clear the canvas.
+  this.ctx_.clearRect(0, 0, this.renderer_.width, this.renderer_.height);
+  
+  // Since the workspace of the device is from -0.05 to +0.05 in all directions,
+  // we make it relative by transforming the renderer workspace which is 0 to
+  // 250. 
+  var x = (this.position_[0] * 10000 + 500) / 4;
+  var y = ((-1 * this.position_[1]) * 10000 + 500) / 4;
+  var z = (this.position_[2] * 1000 + 50) / 4;
+  
+  // The radius of the pointer, to make it visible always start by 2px. The
+  // depth is used for setting the transparency of the pointer, in case we would
+  // like to draw some objects soon.
+  var radius = 2 + z;
+  var depth = Math.abs(radius) / 25;
+  
+  // Draw a circle using plain HTML5 canvas 2D techniques.
+  this.ctx_.strokeStyle = "rgba(0,0,0," + depth + ")";
+  this.ctx_.fillStyle = "rgba(0,0,0," + depth + ")";
+  this.ctx_.beginPath();
+  this.ctx_.arc(x, y, radius, 0, Math.PI * 2, true);
+  this.ctx_.closePath();
+  this.ctx_.stroke();
+  this.ctx_.fill();
 };
 
 /**
@@ -140,7 +186,7 @@ RunnerFactory.prototype._onHapticLoop = function() {
  * @private
  */
 RunnerFactory.prototype._debug = function(msg) {
-  this.bkg_.console.debug(msg);
+  this.console_.debug(msg);
 };
 
 /**
@@ -148,5 +194,5 @@ RunnerFactory.prototype._debug = function(msg) {
  * @private
  */
 RunnerFactory.prototype._error = function(msg) {
-  this.bkg_.console.error(msg);
+  this.console_.error(msg);
 };
