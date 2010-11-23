df24894a52532fa5f379953aa6a5dadbb6676988
Mohamed Mansour <hello@mohamedmansour.com>
2010-11-23 03:28:03 -0500

    Fix license

--- source/haptics_service.cc
+++ source/haptics_service.cc
@@ -1,5 +1,5 @@
 // Copyright 2010 Mohamed Mansour. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can
+// Use of this source code is governed by a GPL license that can
 // be found in the LICENSE file.
 
 #include "haptics_service.h"
